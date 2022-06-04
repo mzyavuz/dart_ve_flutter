91103f38f0c98c58ce071daae5daccd340991b21
M.Zeynep <95376085+mzyavuz@users.noreply.github.com>
2022-06-04 12:09:21 +0300

    5.Ödev güncellendi

--- flutter_dersleri/odev_5_hesap_makinesi/lib/hesap_makinesi.dart
+++ flutter_dersleri/odev_5_hesap_makinesi/lib/hesap_makinesi.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'islemler.dart';
+
 class HesapMakinesi extends StatefulWidget {
   const HesapMakinesi({Key? key}) : super(key: key);
 
@@ -9,11 +11,14 @@ class HesapMakinesi extends StatefulWidget {
 
 class _HesapMakinesiState extends State<HesapMakinesi> {
   double sayi = 0;
+  double sayi2 = 1;
   bool islemeBasildiMi = false;
   bool sayiVarMi = false;
 
+
   @override
   Widget build(BuildContext context) {
+    final islem = Islemler();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hesap Makinesi'),
@@ -39,10 +44,10 @@ class _HesapMakinesiState extends State<HesapMakinesi> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  IslemButonu(isaret: 'AC',),
-                  IslemButonu(isaret: '±',),
-                  IslemButonu(isaret: '%'),
-                  IslemButonu(isaret: '÷'),
+                  IslemButonu(isaret: 'AC', islem: islem.sifirla(sayi),),
+                  IslemButonu(isaret: '±', islem: islem.eksiDegeri(sayi)),
+                  IslemButonu(isaret: '%', islem: islem.yuzdesi(sayi),),
+                  IslemButonu(isaret: '÷', islem: islem.bolme(sayi, sayi2),),
                 ],
               ),
               Row(
@@ -51,74 +56,267 @@ class _HesapMakinesiState extends State<HesapMakinesi> {
                  Expanded(
                    child: Padding(
                      padding: const EdgeInsets.all(8.0),
-                     child: GestureDetector(
-                       onTap: () {
+                     child: TextButton(
+                       onPressed: () {
                          setState(() {
-                           sayi = 7;
+                           sayiVarMi ? sayi = (sayi*10+7) : sayi = 7 ;
                            sayiVarMi = true;
                          });
                        },
-                       child: Container(
-                         decoration: BoxDecoration(
-                           color: Colors.grey,
+                       style: TextButton.styleFrom(
+                         backgroundColor: Colors.grey,
+                         primary: Colors.white70,
+                         shape: const RoundedRectangleBorder(
+                             borderRadius: BorderRadius.all(Radius.circular(20))
                          ),
-                           child: Text('7')
+                       ),
+                       child: const Text('7'),
                        ),
                      ),
                    ),
-                 ),
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
+                      child: TextButton(
                         onPressed: () {
                           setState(() {
-                            sayi = 8;
+                            sayiVarMi ? sayi = (sayi*10+8) : sayi = 8 ;
+                            sayiVarMi = true;
                           });
                         },
-                        child: Text('8'),
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('8'),
                       ),
                     ),
                   ),
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
+                      child: TextButton(
                         onPressed: () {
                           setState(() {
-                            sayi = 9;
+                            sayiVarMi ? sayi = (sayi*10+9) : sayi = 9 ;
+                            sayiVarMi = true;
                           });
                         },
-                        child: Text('9'),
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('9'),
+                      ),
+                    ),
+                  ),
+                  IslemButonu(isaret: 'x', islem: islem.carpma(sayi, sayi2)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sayiVarMi ? sayi = (sayi*10+4) : sayi = 4 ;
+                            sayiVarMi = true;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('4'),
                       ),
                     ),
                   ),
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
+                      child: TextButton(
                         onPressed: () {
                           setState(() {
+                            sayiVarMi ? sayi = (sayi*10+5) : sayi = 5 ;
+                            sayiVarMi = true;
                           });
                         },
-                        child: Text('x'),
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('5'),
                       ),
                     ),
                   ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sayiVarMi ? sayi = (sayi*10+6) : sayi = 6 ;
+                            sayiVarMi = true;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('6'),
+                      ),
+                    ),
+                  ),
+                  IslemButonu(isaret: '-', islem: islem.cikarma(sayi, sayi2)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sayiVarMi ? sayi = (sayi*10+1) : sayi = 1 ;
+                            sayiVarMi = true;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('1'),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sayiVarMi ? sayi = (sayi*10+2) : sayi = 2 ;
+                            sayiVarMi = true;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('2'),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sayiVarMi ? sayi = (sayi*10+3) : sayi = 3 ;
+                            sayiVarMi = true;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('3'),
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(onPressed: () {}, child: Text('+')),
+                  )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sayi = 0;
+                          });
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey,
+                          primary: Colors.white70,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(20))
+                          ),
+                        ),
+                        child: const Text('0'),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                      });
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      primary: Colors.white70,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                    ),
+                    child: const Text(','),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          double sayi1 = sayi;
+                        });
+                      },
+                      child: IslemButonu(isaret: '=', islem: islem.cikarma(sayi, sayi2))),
                 ],
               ),
             ],
           ),
-        ],
-      ),
-    );
+    ]),);
   }
 }
 
 class IslemButonu extends StatefulWidget {
-  String isaret;
-  IslemButonu({
-    Key? key, required this.isaret,
+  final String isaret;
+  final double islem;
+  const IslemButonu({
+    Key? key, required this.isaret, required this.islem,
   }) : super(key: key);
 
   @override
@@ -126,6 +324,7 @@ class IslemButonu extends StatefulWidget {
 }
 
 class _IslemButonuState extends State<IslemButonu> {
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -133,7 +332,8 @@ class _IslemButonuState extends State<IslemButonu> {
         padding: const EdgeInsets.all(8.0),
         child: ElevatedButton(
             onPressed: () {
-              setState( () {
+              setState(() {
+                widget.islem;
               });
             },
             child: Text(
---
+++ flutter_dersleri/odev_5_hesap_makinesi/lib/islemler.dart
@@ -0,0 +1,30 @@
+class Islemler {
+
+  double sifirla(sayi) {
+    return sayi=0.0;
+  }
+
+  double eksiDegeri(sayi) {
+    return sayi=sayi*-1;
+  }
+
+  double yuzdesi(sayi) {
+    return sayi = sayi/100;
+  }
+
+  double bolme(sayi, sayi2) {
+    return sayi = sayi / sayi2;
+  }
+
+  double carpma(sayi, sayi2) {
+    return sayi = sayi * sayi2;
+  }
+
+  double cikarma(sayi, sayi2) {
+    return sayi = sayi - sayi2;
+  }
+
+  double toplama(sayi, sayi2) {
+    return sayi = sayi + sayi2;
+  }
+}
